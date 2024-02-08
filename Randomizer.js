1f73048a31b78bdfaec4d21722e5901a3f812dc8
FreshestInk <83964735+FreshestInk@users.noreply.github.com>
2024-02-07 21:13:40 -0500

    Update Randomizer.js

--- Randomizer.js
+++ Randomizer.js
@@ -390,8 +390,6 @@ function myFunction() {
    // Copy the text inside the text field
   navigator.clipboard.writeText(copyText.value);
 
-  // Alert the copied text
-  alert("Copied the text: " + copyText.value);
 }
 
 
